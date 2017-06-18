dabc0f68740b1b8e363187e23eb1fff40f9fbd56
vicentefleitas <vicente1fleitas1@gmail.com>
2017-06-18 01:47:41 -0400

    test log the path

--- server.js
+++ server.js
@@ -27,5 +27,6 @@ serverr.on('connection', function(socket)
 
 console.log("WebSocket server is running.");
 console.log("Listening to port " + PORT + ".");
+console.log("LocalPath: " + __dirname);
 
 //te amo mucho
