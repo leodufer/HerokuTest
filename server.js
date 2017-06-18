938a89516f10cdd6650a02f590098deeefc6da1f
vicentefleitas <vicente1fleitas1@gmail.com>
2017-06-18 05:23:15 -0400

    chanfe 12

--- server.js
+++ server.js
@@ -3,35 +3,35 @@ var User = require('./game').User;
 var GameRoom = require('./game').GameRoom;
 
 //server code
-const express = require('express');
-const SocketServer = require('ws').Server;
-const path = require('path');
+// const express = require('express');
+// const SocketServer = require('ws').Server;
+// const path = require('path');
 
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+// const PORT = process.env.PORT || 3000;
+// const INDEX = path.join(__dirname, 'index.html');
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+// const server = express()
+//   .use((req, res) => res.sendFile(INDEX) )
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const serverr = new SocketServer({ server });
+// const serverr = new SocketServer({ server });
 
 //NUEVO CODIGOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
-// var WebSocketServer = require("ws").Server
-// var http = require("http")
-// var express = require("express")
-// var app = express()
-// var port = process.env.PORT || 5000
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
 
-// app.use(express.static(__dirname + "/"))
+app.use(express.static(__dirname + "/"))
 
-// var server = http.createServer(app)
-// server.listen(port)
+var server = http.createServer(app)
+server.listen(port)
 
-// var wss = new WebSocketServer({server: server})
+var wss = new WebSocketServer({server: server})
 
 var room1 = new GameRoom();
-serverr.on('connection', function(socket)
+wss.on('connection', function(socket)
 {
 	var user = new User(socket);
 	room1.addUser(user);
